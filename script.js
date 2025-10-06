f01d4e50d46a9533ec5282436b5b4ffcf69db773
GeorgeHliopoulos <hliopoulosgeorgios@gmail.com>
2025-10-06 09:45:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function showGdpr() {
+  fetch('gdpr_terms.txt')
+    .then(response => response.text())
+    .then(text => {
+      document.getElementById('gdpr-text').textContent = text;
+      document.getElementById('gdpr-modal').style.display = 'block';
+    });
+}
 
+function hideGdpr() {
+  document.getElementById('gdpr-modal').style.display = 'none';
+}
