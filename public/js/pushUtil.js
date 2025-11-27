6c6eb4edc2711215eecb10198997afc3b25b85ae
GeorgeHliopoulos <hliopoulosgeorgios@gmail.com>
2025-11-27 08:35:00 +0200

    Delete public directory

--- public/js/pushUtil.js
+++
@@ -1,3 +0,0 @@
-// Provide safe no-op handlers if push code is not fully wired yet.
-window.pushAllow = window.pushAllow || function(){ console.log('pushAllow noop'); };
-window.pushDeny  = window.pushDeny  || function(){ console.log('pushDeny noop'); };
